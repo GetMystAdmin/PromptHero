47ed743eb3d1f663458c41dbb7584065301d1c87
GetMystAdmin <admin@getmyst.app>
2024-05-22 19:19:57 +0000

    first commit

---
+++ src/app/pages/HomePage/index.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react';
+import { Helmet } from 'react-helmet-async';
+import { NavBar } from 'app/components/NavBar';
+import { Masthead } from './Masthead';
+import { Features } from './Features';
+import { PageWrapper } from 'app/components/PageWrapper';
+
+export function HomePage() {
+  return (
+    <>
+      <Helmet>
+        <title>PromptHero</title>
+        <meta
+          name="description"
+          content="An Evolutionary Algorithm Story"
+        />
+      </Helmet>
+      <NavBar />
+      <PageWrapper>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            }}>
+            <div style={{
+                display: 'flex', 
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '50%',
+                borderRight: '1px solid #000',
+                paddingRight: '20px',
+                height: '100%',
+            }}>
+                <input 
+                  type="text" 
+                  placeholder="Enter text here"
+                  style={{
+                    padding: '10px',
+                    fontSize: '1.5em',
+              textAlign: 'center',
+                    width: '70%',
+                    marginBottom: '20px',
+            }}
+                />
+                <button 
+                  type="submit"
+                  style={{
+                    padding: '10px 20px',
+                    fontSize: '1em',
+                    textAlign: 'center',
+                  }}
+                >
+                  Submit
+                </button>
+        </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '50%',
+                paddingLeft: '20px',
+                height: '100%',
+            }}>
+                <div 
+                  style={{
+                    border: '1px dashed #ccc',
+                    width: '70%', 
+                    height: '90vh', 
+                  }}>
+                  Placeholder
+                </div>
+            </div>
+        </div>
+      </PageWrapper>
+
+    </>
+  );
+}
